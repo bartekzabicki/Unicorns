b3c7d8f342ae0625b1a30181e0e0d17692e437cd
Bartek Żabicki <bartekzabicki@gmail.com>
2018-10-08 22:34:19 +0200

    Add UITextView with placeholder as extension

---
+++ Unicorns/Classes/Extensions/UITextViewExtension.swift
@@ -0,0 +1,88 @@
+//
+//  PlaceholderTextView.swift
+//  CompanyManager
+//
+//  Created by Bartek Żabicki on 16.08.2018.
+//  Copyright © 2018 bartekzabicki. All rights reserved.
+//
+
+import UIKit
+
+extension UITextView: UITextViewDelegate {
+  
+  // MARK: - Properties
+  
+  static private let placeholderTag: Int = 1001
+  
+  override open var bounds: CGRect {
+    didSet {
+      resizePlaceholder()
+    }
+  }
+  
+  private var placeholderLabel: UILabel? {
+    return viewWithTag(UITextView.placeholderTag) as? UILabel
+  }
+  
+  @IBInspectable open var localizableKey: String? {
+    get {
+      return nil
+    } set {
+      placeholder = newValue?.localized
+    }
+  }
+  
+  @IBInspectable open var placeholder: String? {
+    get {
+      return placeholderLabel?.text
+    }
+    set {
+      if let placeholderLabel = placeholderLabel {
+        placeholderLabel.text = newValue
+        placeholderLabel.sizeToFit()
+      } else {
+        addPlaceholder(newValue)
+      }
+    }
+  }
+  
+  // MARK: - Functions
+  
+  @objc private func textDidChange(_ sender: NSNotification) {
+    guard let textView = sender.object as? UITextView else { return }
+    placeholderLabel?.isHidden = !textView.text.isEmpty
+  }
+  
+  public func resizePlaceholder() {
+    if let placeholderLabel = placeholderLabel {
+      let labelX = textContainerInset.left
+      let labelY = textContainerInset.top - 2
+      let labelWidth = frame.width - (labelX * 2)
+      let labelHeight = placeholderLabel.frame.height
+      
+      placeholderLabel.frame = CGRect(x: labelX, y: labelY, width: labelWidth, height: labelHeight)
+    }
+  }
+  
+  private func addPlaceholder(_ placeholderText: String?) {
+    guard let placeholderText = placeholderText else { return }
+    let newPlaceholderLabel = UILabel()
+    
+    newPlaceholderLabel.text = placeholderText
+    newPlaceholderLabel.sizeToFit()
+    
+    newPlaceholderLabel.font = font
+    newPlaceholderLabel.textColor = UIColor.lightGray
+    newPlaceholderLabel.tag = UITextView.placeholderTag
+    
+    newPlaceholderLabel.isHidden = text.count > 0
+    
+    addSubview(newPlaceholderLabel)
+    resizePlaceholder()
+    NotificationCenter.default.addObserver(self,
+                                           selector: #selector(textDidChange),
+                                           name: UITextView.textDidChangeNotification,
+                                           object: nil)
+  }
+  
+}
