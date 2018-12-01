31a38629a1ecd448ea5cbdf5e52bef600187ad63
Bartek Zabicki <bartekzabicki@gmail.com>
2018-12-01 21:10:05 +0100

    Add dequaue tableViewCell

--- Unicorns/Classes/Extensions/UITableViewExtension.swift
+++ Unicorns/Classes/Extensions/UITableViewExtension.swift
@@ -14,5 +14,13 @@ extension UITableView {
     let nibName = UINib(nibName: T.reuseIdentifier, bundle: nil)
     register(nibName, forCellReuseIdentifier: T.reuseIdentifier)
   }
+  
+  ///Dequaue reusable cell with identifier equal to cell's name
+  public func dequeueReusable<T: ReuseIdentifying>(cell: T.Type, for indexPath: IndexPath) -> T {
+    guard let cell = dequeueReusableCell(withIdentifier: T.reuseIdentifier, for: indexPath) as? T else {
+      fatalError("Could not dequeue cell with identifier: \(T.reuseIdentifier)")
+    }
+    return cell
+  }
     
 }
