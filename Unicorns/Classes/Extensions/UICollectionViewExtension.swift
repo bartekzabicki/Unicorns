5b38116ae467ca5d2f37d899bbbe443efa72be03
Bartek Zabicki <bartekzabicki@gmail.com>
2018-12-02 12:25:13 +0100

    Add dequaueReusable(cell:) for collectionView

--- Unicorns/Classes/Extensions/UICollectionViewExtension.swift
+++ Unicorns/Classes/Extensions/UICollectionViewExtension.swift
@@ -15,4 +15,12 @@ extension UICollectionView {
     register(nibName, forCellWithReuseIdentifier: T.reuseIdentifier)
   }
   
+  ///Dequaue reusable cell with identifier equal to cell's name
+  public func dequeueReusable<T: ReuseIdentifying>(cell: T.Type, for indexPath: IndexPath) -> T {
+    guard let cell = dequeueReusableCell(withReuseIdentifier: T.reuseIdentifier, for: indexPath) as? T else {
+      fatalError("Could not dequeue cell with identifier: \(T.reuseIdentifier)")
+    }
+    return cell
+  }
+  
 }
