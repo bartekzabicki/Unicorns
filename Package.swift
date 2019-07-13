83e1b3762892fcfae75d9b3cc41013f38f91240d
Bartek Żabicki <bartekzabicki@gmail.com>
2019-07-13 17:58:32 +0200

    Missing Package.swift

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Unicorns",
+    platforms: [.iOS(.v10)],
+    products: [
+        .library(
+            name: "Unicorns",
+            targets: ["Unicorns"]),
+    ],
+    targets: [
+        .target(
+            name: "Unicorns",
+            dependencies: [],
+            path: ".",
+            sources: ["Unicorns/Classes"])
+    ]
+)
