465d072d8addd113e2ced1a0e376775b2aef7987
Bartek Żabicki <bartekzabicki@gmail.com>
2019-08-28 17:06:58 +0200

    Add macOS support from v10.13

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Unicorns",
-    platforms: [.iOS(.v10)],
+    platforms: [.iOS(.v10), .macOS(.v10_13)],
     products: [
         .library(
             name: "Unicorns",
